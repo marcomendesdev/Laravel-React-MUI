33ed69487958c48fadad271de0c0aa1fdd37b027
Marco <mendesdevuk@gmail.com>
2023-06-09 11:52:40 +0100

    none

--- database/migrations/2023_06_01_143346_create_items_table.php
+++ database/migrations/2023_06_01_143346_create_items_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('name');
             $table->longText('description');
             $table->decimal('price', 8, 2);
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
